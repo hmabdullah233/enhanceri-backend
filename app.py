806d043ae52f195a2e5aaf7d4af4d2432cd3d2ff
hmabdullah233 <biographyzon@gmail.com>
2025-04-26 21:59:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,6 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from utils.image_utils import full_enhance
 
-# --- Weight Downloading Setup ---
-def download_file(url, destination):
-    os.makedirs(os.path.dirname(destination), exist_ok=True)
-    if not os.path.exists(destination):
-        print(f"Downloading {os.path.basename(destination)}...")
-        urllib.request.urlretrieve(url, destination)
-        print(f"Downloaded {os.path.basename(destination)} successfully.")
-    else:
-        print(f"{os.path.basename(destination)} already exists. Skipping download.")
-
-# CodeFormer model weight
-download_file(
-    "https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth",
-    "models/codeformer/weights/codeformer/codeformer.pth"
-)
-
-# FaceLib ArcFace model weight
-download_file(
-    "https://huggingface.co/henry/arcface-resnet50/resolve/main/arcface_resnet50.pth",
-    "models/codeformer/weights/facelib/arcface.pth"
-)
-# --- Weight Downloading Setup Ends ---
-
 app = FastAPI()
 
 @app.post("/enhance")
