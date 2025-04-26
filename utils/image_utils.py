ffcd5f270f5240e8a3ab3480840aa7a005a1f195
hmabdullah233 <biographyzon@gmail.com>
2025-04-26 22:01:18 +0500

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -4,7 +4,6 @@ import torch
 import numpy as np
 
 from models.realesrgan.main import apply_realesrgan
-from models.codeformer.main import apply_codeformer
 from models.bsrgan.main import apply_bsrgan
 from models.swinir.main import apply_swinir
 from models.enlightengan.main import apply_enlightengan
@@ -13,7 +12,6 @@ from models.dce.main import apply_dce
 def full_enhance(input_path, output_path):
     img = cv2.imread(input_path)
 
-    img = apply_codeformer(img)
     img = apply_bsrgan(img)
     img = apply_realesrgan(img)
     img = apply_swinir(img)
