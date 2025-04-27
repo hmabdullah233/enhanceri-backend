5e8ce6400ba5bed903cd2aced9f89c4caadbeff1
hmabdullah233 <biographyzon@gmail.com>
2025-04-27 09:32:02 +0200

    Update rrdbnet_model.py

--- models/basicsr/models/rrdbnet_model.py
+++ models/basicsr/models/rrdbnet_model.py
@@ -1,1 +1,9 @@
+import torch.nn as nn
 
+class RRDBNetModel(nn.Module):
+    def __init__(self, model):
+        super(RRDBNetModel, self).__init__()
+        self.model = model
+
+    def forward(self, x):
+        return self.model(x)
