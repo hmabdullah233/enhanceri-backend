987e7eead2f68d4e290fae39b628090b5ea467ae
hmabdullah233 <biographyzon@gmail.com>
2025-04-27 09:30:17 +0200

    Update rrdbnet_arch.py

--- models/basicsr/archs/rrdbnet_arch.py
+++ models/basicsr/archs/rrdbnet_arch.py
@@ -1,1 +1,18 @@
+import torch
+import torch.nn as nn
 
+class RRDBNet(nn.Module):
+    def __init__(self, num_in_ch, num_out_ch, num_feat, num_block, num_grow_ch, scale):
+        super(RRDBNet, self).__init__()
+        self.scale = scale
+        self.body = nn.Sequential(
+            *[nn.Conv2d(num_feat, num_feat, 3, padding=1) for _ in range(num_block)]
+        )
+        self.head = nn.Conv2d(num_in_ch, num_feat, 3, padding=1)
+        self.tail = nn.Conv2d(num_feat, num_out_ch, 3, padding=1)
+
+    def forward(self, x):
+        x = self.head(x)
+        x = self.body(x)
+        x = self.tail(x)
+        return x
