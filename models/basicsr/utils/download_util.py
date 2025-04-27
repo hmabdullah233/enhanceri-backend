3d65d8791de6d2eacee4e7f3103a7953b26c8b23
hmabdullah233 <biographyzon@gmail.com>
2025-04-27 09:32:55 +0200

    Update download_util.py

--- models/basicsr/utils/download_util.py
+++ models/basicsr/utils/download_util.py
@@ -1,1 +1,13 @@
+import os
+import requests
 
+def load_file_from_url(url, model_dir='weights', progress=True):
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    filename = url.split('/')[-1]
+    file_path = os.path.join(model_dir, filename)
+    if not os.path.isfile(file_path):
+        r = requests.get(url, allow_redirects=True)
+        with open(file_path, 'wb') as f:
+            f.write(r.content)
+    return file_path
